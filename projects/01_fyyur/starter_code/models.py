29ae6a3a483b2cbd2b18d87cc58e70bc490d144c
Oubri Daouda <64691056+oubridaouda@users.noreply.github.com>
2022-08-16 02:15:17 +0000

    Create models.py

---
+++ projects/01_fyyur/starter_code/models.py
@@ -0,0 +1,41 @@
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    genres = db.Column(db.String)
+    website_link = db.Column(db.String)
+    seeking_talent = db.Column(db.String)
+    seeking_description = db.Column(db.String)
+    shows = db.relationship('Show', backref='venues', lazy=True)
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    genres = db.Column(db.String)
+    website_link = db.Column(db.String)
+    seeking_venue = db.Column(db.String)
+    seeking_description = db.Column(db.String)
+    shows = db.relationship('Show', backref='artists', lazy=True)
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+    __tablename__ = 'Show'
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'),nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'),nullable=False)
+    start_time = db.Column(db.String(100))
